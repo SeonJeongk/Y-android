e99305e880d8193994c4066e3c16713cb67adc0e
gayeon7877 <gayean01@gmail.com>
2024-01-03 01:34:02 +0900

    🐛Fix:팀원관리페이지

--- app/src/main/java/kr/ac/duksung/hackathon_y/ui/TeamManagement/TeamMemberRecAdapter.kt
+++ app/src/main/java/kr/ac/duksung/hackathon_y/ui/TeamManagement/TeamMemberRecAdapter.kt
@@ -3,6 +3,7 @@ package kr.ac.duksung.hackathon_y.ui.TeamManagement
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import kr.ac.duksung.hackathon_y.R
 import kr.ac.duksung.hackathon_y.databinding.ItemTeammemberBinding
 import kr.ac.duksung.hackathon_y.ui.TeamManagement.dto.TeamMemberDto
 
@@ -13,7 +14,18 @@ class TeamMemberRecAdapter(private var teammemberList:List<TeamMemberDto>):Recyc
     class TeamMemberViewHolder(private val binding: ItemTeammemberBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(teammember: TeamMemberDto) {
             binding.name.text = teammember.title
+            // 이미지 설정
+            if (teammember.imageResId != null) {
+                binding.profile.setImageResource(teammember.imageResId!!)
+            } else {
+                // 기본 이미지나 플레이스홀더 이미지 설정
+                binding.profile.setImageResource(R.drawable.profile_image01)
+            }
+            val result = 100*((teammember.totaltask?.toDouble() ?: 0.0) / teammember.finishedtask!!)
+            binding.percent.text = "%.0f".format(result)
 
+            binding.progressBar.max = 100 // 프로그레스바의 최대값을 100으로 설정
+            binding.progressBar.progress = ((teammember.finishedtask?.toDouble() ?: 0.0) / teammember.totaltask!! * 100).toInt() // 프로그레스바의 현재 상태를 계산하여 설정
         }
     }
 
--- app/src/main/java/kr/ac/duksung/hackathon_y/ui/TeamManagement/dto/TeamMemberDto.kt
+++ app/src/main/java/kr/ac/duksung/hackathon_y/ui/TeamManagement/dto/TeamMemberDto.kt
@@ -1,8 +1,9 @@
 package kr.ac.duksung.hackathon_y.ui.TeamManagement.dto
 
 data class TeamMemberDto(
-    var title: String? =null,
-    var totaltask:Int?=null,
-    var finishedtask:Int?=null
-
+    var title: String? = null,
+    var totaltask: Int? = null,
+    var finishedtask: Int? = null,
+    var imageResId: Int? = null // 이미지 URL을 저장할 속성 추가
 )
+
