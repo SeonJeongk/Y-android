edecdf81a8ec6b79c1037d16d02af3ec622a6e51
dkyuuum <dkyuuum@gmail.com>
2024-01-02 18:36:06 +0900

    feat: 프로젝트 초기 설정

---
+++ app/src/main/java/kr/ac/duksung/hackathon_y/MainActivity.kt
@@ -0,0 +1,50 @@
+package kr.ac.duksung.hackathon_y
+
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Base64
+import android.util.Log
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kr.ac.duksung.hackathon_y.ui.MeetingFragment
+import java.security.MessageDigest
+
+class MainActivity : AppCompatActivity() {
+
+    private val meetingFragment = MeetingFragment()
+    private lateinit var bottomNavigationView: BottomNavigationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView)
+
+        // MainFragment로 시작
+        replaceFragment(meetingFragment)
+
+        bottomNavigationView.setOnItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.tapMeeting -> {
+                    replaceFragment(meetingFragment)
+                }
+                R.id.tapTeam -> {
+                }
+                R.id.tapAlram -> {
+                }
+                R.id.tapSchedule -> {
+                }
+
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainer, fragment)
+            .commit()
+    }
+}
