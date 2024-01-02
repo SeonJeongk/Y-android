ec5c0e6a988290deb69de924ce9fdebc33bfdf62
gayeon7877 <gayean01@gmail.com>
2024-01-03 04:38:19 +0900

    ✨Feat: 프로젝트 페이지02

--- app/src/main/java/kr/ac/duksung/hackathon_y/ui/Project/AddProjectFragment.kt
+++ app/src/main/java/kr/ac/duksung/hackathon_y/ui/Project/AddProjectFragment.kt
@@ -5,14 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import kr.ac.duksung.hackathon_y.R
 import kr.ac.duksung.hackathon_y.databinding.FragmentAddProjectBinding
+import kr.ac.duksung.hackathon_y.ui.TeamManagement.TeamRecAdapter
 
 
 class AddProjectFragment : Fragment() {
 
     private var _binding: FragmentAddProjectBinding? = null
     private val binding get() = _binding!!
+    private lateinit var viewModel: ProjectViewModel
+    private lateinit var memberAdapter: MemberRecAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,13 +31,29 @@ class AddProjectFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentAddProjectBinding.inflate(inflater, container, false)
+        viewModel = ViewModelProvider(this).get(ProjectViewModel::class.java)
+
+        // MemberRecAdapter를 초기화합니다.
+        memberAdapter = MemberRecAdapter(listOf())
+
+        // RecyclerView에 Adapter를 설정합니다.
+        binding.addmemRec.adapter = memberAdapter
+
+        // RecyclerView의 레이아웃 매니저를 설정합니다.
+        binding.addmemRec.layoutManager = LinearLayoutManager(context,LinearLayoutManager.HORIZONTAL, false)
+
+
+        viewModel.MemberDtos.observe(viewLifecycleOwner,{
+                memberList->
+            memberAdapter.updateList(memberList)
+        })
         return binding.root
+
+
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
-
-
 }
